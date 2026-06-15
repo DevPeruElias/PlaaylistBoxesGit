9acdf1b1ccb836b476b1343ac2cf56f984649486
Elias Mirez <mirezelias4@gmail.com>
2026-06-15 16:07:22 -0500

    Prueba SessionId

--- src/app/core/models/box-state.interface.ts
+++ src/app/core/models/box-state.interface.ts
@@ -3,6 +3,7 @@ import { Cancion } from './cancion.interface';
 export interface BoxState {
   sede: string;
   boxId: string;
+  sessionId: string; // Cambia cuando el admin reinicia el box
   estadoReproduccion: 'idle' | 'playing' | 'paused';
   cancionActual: Cancion | null;
   playlist: Cancion[];
